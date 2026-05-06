153881bfcad1736588b923d51c382556f53a23e4
Archit Mishra <58510411+avm3005@users.noreply.github.com>
2026-05-06 22:35:08 +0530

    Delete Uninstall.bat

--- Uninstall.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-:: Launch Uninstall as Admin
-powershell.exe -Command "Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -NoProfile -File "%~dp0ps1s\Uninstall-AutoDM.ps1"' -Verb RunAs"
